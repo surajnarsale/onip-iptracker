357c8adcab99f10a0190c0efcdc3a484920ff30f
Suraj Narsale <surajsnarsale@gmail.com>
2021-09-23 09:16:44 +0530

    feat: add bg image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-  purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			backgroundImage: (theme) => ({
+				'hero-pattern': "url('pattern-bg.png')",
+			}),
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
