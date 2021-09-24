ca52e2e5ce72cb6173acfb1f2601087720b9dad6
Suraj Narsale <surajsnarsale@gmail.com>
2021-09-24 09:08:59 +0530

    feat: add search component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex flex-col h-screen max-h-screen ">
+    <!-- Search / Results -->
+    <div
+      class="flex justify-center relative bg-hero-pattern bg-cover px-4 pt-8 pb-32 "
+    >
+      <!-- Search Input -->
+      <div class="w-full max-w-screen-sm ">
+        <h1 class="text-white text-center text-3xl pb-4 ">
+          IP Address Tracker
+        </h1>
+        <div class="flex">
+          <input
+            class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none"
+            type="text"
+            placeholder="Search for any IP address or leave empty to get your ip info"
+          />
+          <i
+            @click="getIpInfo"
+            class="cursor-pointer
+          bg-black
+          text-white
+          px-4
+          rounded-tr-md
+          rounded-br-md
+          flex
+          items-center
+          fas fa-chevron-right"
+          ></i>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {}
+};
 </script>
