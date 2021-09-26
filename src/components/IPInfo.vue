d80749ef0fe582adcf6f7f16ece59acf1ecfcb2d
Suraj Narsale <surajsnarsale@gmail.com>
2021-09-26 06:24:41 +0530

    feat: add map component

--- src/components/IPInfo.vue
+++ src/components/IPInfo.vue
@@ -9,29 +9,29 @@
     bg-white 
     gap-y-4 
     gap-x-12 
-    px-11 pt-8 
+    px-8 pt-8 
     pb-12 
     rounded-md"
   >
     <!-- Flex Item #1  -->
     <div class="flex flex-col">
       <h3 class="text-xs mb-2 uppercase">IP Address</h3>
-      <span class="text-2xl">8.8.8.8</span>
+      <span class="text-2xl">{{ ipInfo.address }}</span>
     </div>
     <!-- Flex Item #2  -->
     <div class="flex flex-col">
       <h3 class="text-xs mb-2 uppercase">Location</h3>
-      <span class="text-2xl">Michigan</span>
+      <span class="text-2xl">{{ ipInfo.state }}</span>
     </div>
     <!-- Flex Item #3  -->
     <div class="flex flex-col">
       <h3 class="text-xs mb-2 uppercase">Timezone</h3>
-      <span class="text-2xl">UTC -7:00</span>
+      <span class="text-2xl">UTC {{ ipInfo.timezone }}</span>
     </div>
     <!-- Flex Item #4  -->
     <div class="flex flex-col">
       <h3 class="text-xs mb-2 uppercase">Isp</h3>
-      <span class="text-2xl capitalize">Google</span>
+      <span class="text-2xl capitalize">{{ ipInfo.isp }}</span>
     </div>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,15 @@
 <template>
-  <div class="flex flex-col h-screen max-h-screen ">
+  <div class="flex flex-col h-screen max-h-screen">
     <!-- Search / Results -->
     <div
-      class="flex justify-center relative bg-hero-pattern bg-cover px-4 pt-8 pb-32 "
+      class="z-20 flex justify-center relative bg-hero-pattern bg-cover px-4 pt-8 pb-32"
     >
       <!-- Search Input -->
-      <div class="w-full max-w-screen-sm ">
-        <h1 class="text-white text-center text-3xl pb-4 ">
-          IP Address Tracker
-        </h1>
+      <div class="w-full max-w-screen-sm">
+        <h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
         <div class="flex">
           <input
+            v-model="queryIp"
             class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none"
             type="text"
             placeholder="Search for any IP address or leave empty to get your ip info"
@@ -29,17 +28,73 @@
           ></i>
         </div>
       </div>
-      <!-- IP info -->
-      <IPInfo />
+      <!-- IP Info -->
+      <IPInfo v-if="ipInfo" v-bind:ipInfo="ipInfo" />
     </div>
+
+    <!-- Map -->
+    <div id="mapid" class="h-full z-10"></div>
   </div>
 </template>
 
 <script>
+import IPInfo from "../components/IPInfo.vue";
+import leaflet from "leaflet";
+import { onMounted, ref } from "vue";
+import axios from "axios";
 
-import IPInfo from '../components/IPInfo.vue'
+
+
 export default {
   name: "Home",
-  components: {IPInfo}
+  components: { IPInfo },
+  setup() {
+    // create map variable
+    let mymap;
+    // data
+    const queryIp = ref("");
+    const ipInfo = ref(null);
+    // mounted lifecycle hook, creates the map
+    onMounted(() => {
+      mymap = leaflet.map("mapid").setView([42.5145, -83.0147], 9);
+      leaflet
+        .tileLayer(
+          "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3VyYWpuYXJzYWxlIiwiYSI6ImNrdTI1dmhmNTE5c3IybnBjcWIxcXdnaWsifQ.t5hD8RXIOdx76xjO2apZ9w",
+          {
+            attribution:
+              'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: "mapbox/streets-v11",
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken:
+              "pk.eyJ1Ijoic3VyYWpuYXJzYWxlIiwiYSI6ImNrdTI1dmhmNTE5c3IybnBjcWIxcXdnaWsifQ.t5hD8RXIOdx76xjO2apZ9w",
+          }
+        )
+        .addTo(mymap);
+    });
+    // gets ip information from API
+    const getIpInfo = async () => {
+      try {
+        const data = await axios.get(
+          `https://geo.ipify.org/api/v1?apiKey=at_fTwauXXCo4P43klctOAOYPP2QhFMF&ipAddress=${queryIp.value}`
+        );
+        const result = data.data;
+        ipInfo.value = {
+          address: result.ip,
+          state: result.location.region,
+          timezone: result.location.timezone,
+          isp: result.isp,
+          lat: result.location.lat,
+          lng: result.location.lng,
+        };
+        leaflet.marker([ipInfo.value.lat, ipInfo.value.lng]).addTo(mymap);
+        mymap.setView([ipInfo.value.lat, ipInfo.value.lng], 13);
+      } catch (err) {
+        alert(err.message);
+      }
+    };
+    return { queryIp, ipInfo, getIpInfo };
+  },
 };
 </script>
